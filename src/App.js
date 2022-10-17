f287a2eb9e9191156a9d3acf807597dbf94b3e7c
MAbozed <mabozed.com@gmail.com>
2022-10-17 22:05:26 +0300

    Header Component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import { BrowserRouter as Router,Routes,Route,Link} from "react-router-dom"
 import "./App.css"
+import Header from "./components/common/header/Header"
 function App() {
   return (
     <>
-    
+      <Router>
+        <Header />
+        <Routes>
+            {/* <Route /> */}
+        </Routes>
+      </Router>
     </>
   )
 }
